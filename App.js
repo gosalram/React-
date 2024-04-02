cbf2da364f1569c33efe94bcead0fee7f7e3a84e
gosalram <138030010+gosalram@users.noreply.github.com>
2024-04-02 10:51:08 +0530

    lesson 1

---
+++ App.js
@@ -0,0 +1,27 @@
+/* const heading = React.createElement(
+    "h1",
+    { id:"heading",xyx:"abc"},
+    "Hello from React"
+    );
+
+    console.log(heading) */
+const parent =React.createElement(
+"div",
+{id:"parent"},
+[React.createElement(
+    "div",
+    {id:"child"},
+    [
+        React.createElement("h1",{},"Im h1 tag"),
+        React.createElement("h2",{},"Im h2 tag"),
+    ]),
+    React.createElement(
+        "div",
+        {id:"child2"},
+        [
+            React.createElement("h1",{},"Im h1 tag"),
+            React.createElement("h2",{},"Im h2 tag"),
+        ])
+]);    
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
